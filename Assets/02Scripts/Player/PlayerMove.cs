9ac600a95b1da019826c92b2856508f1ba67b9a8
ctmanjak <ctmanjak@gmail.com>
2025-11-03 13:41:56 +0900

    1. 제한된 영역 동작 2. 스피드 조작

--- Assets/02Scripts/Player/PlayerMove.cs
+++ Assets/02Scripts/Player/PlayerMove.cs
@@ -2,10 +2,23 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float speed = 3;
+    public Camera mainCamera;
+    private Vector2 _movableTopLeft;
+    private Vector2 _movableBottomRight;
+    
+    public float speed = 5f;
+    public float maxSpeed = 10f;
+    public float minSpeed = 3f;
+    public float speedChangeAmount = 5f;
     void Start()
     {
-        
+        if (mainCamera == null) mainCamera = Camera.main;
+
+        if (mainCamera != null)
+        {
+            _movableTopLeft = mainCamera.ViewportToWorldPoint(new Vector3(0, 0.5f, mainCamera.nearClipPlane));
+            _movableBottomRight = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, mainCamera.nearClipPlane));
+        }
     }
 
     void Update()
@@ -15,12 +28,21 @@ public class PlayerMove : MonoBehaviour
         
         Debug.Log($"h: {h}, v: {v}");
 
-        Vector2 direction = new Vector2(h, v);
+        Vector2 direction = new Vector2(h, v) * (speed * Time.deltaTime);
         Debug.Log($"direction: ({direction.x}, {direction.y})");
 
         Vector2 position = transform.position;
-        Vector2 newPosition = position + direction * (speed * Time.deltaTime);
 
+        Vector2 newPosition = new Vector2(
+            Mathf.Clamp(position.x + direction.x, _movableTopLeft.x, _movableBottomRight.x),
+            Mathf.Clamp(position.y + direction.y, _movableBottomRight.y, _movableTopLeft.y)
+        );
+        
+        Debug.Log($"newPosition: ({newPosition.x}, {newPosition.y})");
+
+        if (Input.GetKey(KeyCode.Q)) speed = Mathf.Min(speed + speedChangeAmount * Time.deltaTime, maxSpeed);
+        else if (Input.GetKey(KeyCode.E)) speed = Mathf.Max(speed - speedChangeAmount * Time.deltaTime, minSpeed);
+        
         transform.position = newPosition;
     }
 }
