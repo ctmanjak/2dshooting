32d6f0c1e026d1e846f88281fe82e1f179b9432d
ctmanjak <ctmanjak@gmail.com>
2025-11-12 12:45:01 +0900

    feat: 점수 저장, 불러오기 추가

--- Assets/02Scripts/Score/ScoreManager.cs
+++ Assets/02Scripts/Score/ScoreManager.cs
@@ -8,10 +8,15 @@ namespace _02Scripts.Score
         [SerializeField] private Text _scoreTextUI;
 
         private int _score = 0;
+
+        private const string ScoreKey = "Score";
         
         private void Start()
         {
             if (!_scoreTextUI) return;
+            
+            Load();
+            
             UpdateScoreText();
         }
 
@@ -19,12 +24,25 @@ namespace _02Scripts.Score
         {
             if (amount < 1) return;
             _score += amount;
+            
             UpdateScoreText();
+            
+            Save();
         }
 
         private void UpdateScoreText()
         {
             _scoreTextUI.text = $"현재 점수 : {_score:N0}";
+        }
+        
+        private void Save()
+        {
+            PlayerPrefs.SetInt(ScoreKey, _score);
+        }
+
+        private void Load()
+        {
+            _score = PlayerPrefs.GetInt(ScoreKey);
         }
     }
 }
